bb4230e8645efdc4bc690e3dfb2d23b24c6fc7a9
Zamiz <zamiztobar@gmail.com>
2018-12-04 20:31:50 -0300

    routes: add ', only [:create]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories
+  resources :categories, only: [:create]
   root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
