fda98cc3bae1932b0d16ec910fcee9303080dfb4
Zamiz <zamiztobar@gmail.com>
2018-12-04 21:55:55 -0300

    +@product to show method in categories controller *create method +category show view with form +nested routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,17 @@
 class ProductsController < ApplicationController
+    
+    def create
+        @product.category_id = @category.id
+        @category = Category.find(params[:category_id])
+        @product = Product.new(product_params)
+        if @product.save
+            @category = @product.category
+            redirect_to category_path
+        else
+            redirect_to @category
+        end
+    end
+    
     private
 
     def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories
+  # resources :categories
+
+  resources :categories do
+    resources :products
+  end
+
   resources :products, only: [:create]
   root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
