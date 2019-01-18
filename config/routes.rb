a89c77b206847cd5d11dc84130e947935d12ec76
Zamiz <zamiztobar@gmail.com>
2019-01-18 19:12:51 -0300

    +add product movie form to views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # resources :categories
 
   resources :categories do
-    resources :products
+    resources :products, only: [:create, :destroy]
   end
 
   resources :products, only: [:create]
